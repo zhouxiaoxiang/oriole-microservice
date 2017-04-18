db98aca680218ee01aeb69261424cb22caa26fd2
Eric <xiaoxiang.cn@gmail.com>
2017-04-18 22:38:35 +0800

    Add demo

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open(os.path.join(here, 'README.md'), 'r', 'utf-8') as handle:
 
 setup(
     name='oriole-service',
-    version='3.0.2',
+    version='3.0.3',
     description='Rapidly create services.',
     long_description=readme,
     author='Eric.Zhou',
