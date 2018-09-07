cb3aed1ac8b64db1ff1a49484d28bc5b85ad3f2b
Eric <xiaoxiang.cn@gmail.com>
2018-09-07 13:44:54 +0800

    16.2.0 => 17.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 
 setup(
     name='oriole-service',
-    version='16.2.0',
+    version='17.0.0',
     description='Rapidly create services.',
     long_description=open('README.rst').read(),
     author='Eric.Zhou',
