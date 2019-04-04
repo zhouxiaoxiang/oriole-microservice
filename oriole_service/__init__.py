fecd29e66fa5f6f88959fbbd1325a5b6ead65fd6
Eric <xiaoxiang.cn@gmail.com>
2019-04-04 11:14:53 +0800

    20.4.0 => 21.0.0

--- oriole_service/__init__.py
+++ oriole_service/__init__.py
@@ -1,5 +1,5 @@
 from oriole.vos import set_loc
 
-__version__ = '20.4.0'
+__version__ = '21.0.0'
 
 set_loc()
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 
 setup(
     name='oriole-service',
-    version='20.4.0',
+    version='21.0.0',
     description='Rapidly create services.',
     long_description=open('README.rst').read(),
     author='Eric.Zhou',
