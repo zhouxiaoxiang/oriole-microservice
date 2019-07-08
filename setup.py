a374d64c143ca410f6d49a095f3911034ffc45ab
Eric <xiaoxiang.cn@gmail.com>
2019-07-08 11:21:53 +0800

    Do not notify.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 
 setup(
     name='oriole-service',
-    version='29.5.0',
+    version='29.6.0',
     description='Rapidly create services.',
     long_description=open('README.rst').read(),
     author='Eric.Zhou',
